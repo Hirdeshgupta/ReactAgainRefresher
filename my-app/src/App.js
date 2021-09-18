11aea9031049d0ff0be4d9138796b17b3bf6c98d
Hirdeshgupta <hirdeshgupta16@gmail.com>
2021-09-18 16:09:59 +0530

    Folder Structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
-
+import Header from "./Components/Header/Header";
+import Lists from "./Components/Lists/Lists";
+import Map from "./Components/Map/Map";
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
 const App =()=>{
     return(
-        <h1>Hello World !</h1>
+        <>
+         <CssBaseline/>
+         <Header/>
+         <Grid container spacing={4} >
+             <Grid item xs={12} md={4} >
+                 <Lists/>
+             </Grid>
+             <Grid item xs={12} md={8}>
+                 <Map/>
+             </Grid>
+         </Grid>   
+        </>
     )
 }
 export default App;
---
+++ my-app/src/Components/PlaceDetails/PlaceDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlaceDetails = () => {
+    return (
+        <div>
+            Map
+        </div>
+    )
+}
+
+export default PlaceDetails
